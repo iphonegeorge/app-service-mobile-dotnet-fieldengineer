bcb37f1258246fd738fdd52f16b1aa42cc3c04bb
George Mirabelli <george.mirabelli@gmaill.com>
2019-02-15 20:43:03 +1100

    MainActivity.cs changes to try and get to compile

--- client/FieldEngineerLite.Android/MainActivity.cs
+++ client/FieldEngineerLite.Android/MainActivity.cs
@@ -1,6 +1,12 @@
-﻿using Android.App;
+﻿using System;
+
+using Android.App;
 using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
 using Android.OS;
+
 
 
 namespace FieldEngineerLite.Droid
@@ -10,10 +16,13 @@ namespace FieldEngineerLite.Droid
         MainLauncher = true,
         ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
         Theme = "@android:style/Theme.Holo.Light")]
-    public class MainActivity : FormsAppCompatActivity
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
             base.OnCreate(bundle);
 
             Xamarin.Forms.Forms.Init(this, bundle);
